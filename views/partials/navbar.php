590898def353a790cb236af05db5d99f99a9493b
Mansib Rayhan Reza <saminrayhanreza@gmail.com>
2026-05-19 05:56:16 +0600

    Update repo with new files

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -1,32 +1,27 @@
-<div class="navbar">
+<?php
+// Expect bootSession() already called by parent view.
+$navRole       = $_SESSION["role"] ?? "";
+$navLoggedIn   = isset($_SESSION["user_id"]);
+$navIsVerified = (int) ($_SESSION["is_verified"] ?? 0) === 1;
+?>
+<nav class="site-nav">
+    <a href="../home/index.php" class="nav-brand">🌍 Travel Guide</a>
 
-<a href="../home.php">
-Home
-</a>
+    <div class="nav-links">
+        <a href="../home/index.php">Home</a>
 
-<a href="../profile/profile.php">
-Profile
-</a>
-
-<a href="../wishlist/wishlist.php">
-Wishlist
-</a>
-
-<a href="../../controllers/LogoutController.php">
-Logout
-</a>
-
-</div>
-
-<script>
-(function(){
-  var t = document.getElementById('navToggle');
-  var l = document.getElementById('navLinks');
-  if(t && l){
-    t.addEventListener('click', function(){
-      var open = l.classList.toggle('open');
-      t.setAttribute('aria-expanded', open ? 'true' : 'false');
-    });
-  }
-})();
-</script>
+        <?php if (!$navLoggedIn): ?>
+            <a href="../auth/login.php">Login</a>
+            <a href="../auth/register.php">Register</a>
+        <?php else: ?>
+            <?php if ($navRole === "scout" && $navIsVerified): ?>
+                <a href="../scout/dashboard.php">Scout Dashboard</a>
+            <?php endif; ?>
+            <?php if ($navRole === "user" && $navIsVerified): ?>
+                <a href="../wishlist/index.php">Wishlist</a>
+            <?php endif; ?>
+            <a href="../profile/profile.php">Profile</a>
+            <a href="../auth/logout.php">Logout</a>
+        <?php endif; ?>
+    </div>
+</nav>
