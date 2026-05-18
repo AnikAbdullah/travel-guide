d47aeb9f0265f94e2865ff0c349776420eeb8ea0
Abdullah Al Taieb <abdullah.al.taie.b.02@gmail.com>
2026-05-18 22:11:13 +0600

    Minimise footer

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -4,23 +4,6 @@
 <div class="toast-container" id="toastContainer"></div>
 
 <footer class="site-footer">
-    <div class="footer-inner">
-
-        <div class="footer-brand">
-            <h3>🌍 Travel Guide</h3>
-            <p>Collecting and sharing the world's most beautiful places, one scout submission at a time.</p>
-        </div>
-
-        <nav class="footer-nav">
-            <a href="dashboard.php">Dashboard</a>
-            <a href="create_request.php">New Request</a>
-            <a href="my_requests.php">My Requests</a>
-            <a href="approved_posts.php">Approved Posts</a>
-            <a href="#top">↑ Top</a>
-        </nav>
-
-    </div>
-
     <p class="copyright">&copy; <?= date('Y') ?> Travel Guide &mdash; Scout Module &mdash; 22-48028-2</p>
 </footer>
 
