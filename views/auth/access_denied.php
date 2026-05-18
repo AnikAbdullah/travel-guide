e0543e8eb40d6ba60a37a0bf4536057e712f5bc3
Mansib Rayhan Reza <saminrayhanreza@gmail.com>
2026-05-19 05:56:16 +0600

    Update repo with new files

--- views/auth/access_denied.php
+++ views/auth/access_denied.php
@@ -1,37 +1,44 @@
+<?php
+
+require_once "../../helpers/auth.php";
+
+bootSession();
+
+function e($value)
+{
+    return htmlspecialchars($value ?? "");
+}
+
+$pageTitle = "Access Denied";
+
+?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= e($pageTitle) ?> | Travel Guide</title>
 
-<title>Access Denied</title>
-
-<link
-rel="stylesheet"
-href="../../public/css/style.css"
->
-
+    <link rel="stylesheet" href="../../public/css/auth.css">
 </head>
 <body>
 
-<div class="container">
+<?php require_once "../partials/navbar.php"; ?>
 
-<h1>
-Access Denied
-</h1>
+<main class="auth-container">
 
-<p>
-You Do Not Have Permission
-To Access This Page
-</p>
+    <h2>Access Denied</h2>
 
-<a href="../home.php">
+    <div class="alert alert-info">
+        Your account is pending admin approval
+        or you do not have permission to access this page.
+    </div>
 
-<button>
-Return Home
-</button>
+    <a class="home-btn" href="../home/index.php">
+        Back To Home
+    </a>
 
-</a>
-
-</div>
+</main>
 
 </body>
 </html>
