2ea9c3975bbb5e156b2e88ac86ae4298f88dc35e
Mansib Rayhan Reza <saminrayhanreza@gmail.com>
2026-05-19 06:15:25 +0600

    Update repo with new files

--- index.php
+++ index.php
@@ -1,16 +1,8 @@
 <?php
-
 session_start();
-
-if(isset($_SESSION['user_id'])){
-
-    header("Location: views/home.php");
-
-}else{
-
+if (isset($_SESSION["user_id"])) {
+    header("Location: views/home/index.php");
+} else {
     header("Location: views/auth/login.php");
 }
-
-exit();
-
-?>
+exit;
